dd3c06513b73b551e367c6128cd106104177cb27
Holly Valenty <holly914@gmail.com>
2018-01-29 17:26:31 -0600

    Fancy Array Assignment

---
+++ Fancy_Array_Assignment/script.js
@@ -0,0 +1,8 @@
+function fancyArray(arr){ //defining the function. 
+
+for(var i=0; i < arr.length; i++){
+    console.log(i + "-> " + arr[i])}
+}
+var myArr = ["James", "Jill", "Jane", "Jack"] //sample data to use with it
+
+fancyArray(myArr); //calling the function defined above, with "myArr" as ingredient
