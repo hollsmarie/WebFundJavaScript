1ad5f7f75bc4caaa756bfa5d85defba81123b199
Holly Valenty <holly914@gmail.com>
2018-01-30 15:31:44 -0600

    Added Disappearing Ninja Assignment

---
+++ jQuery_Assignments/jQuery_Disappearing_Ninja/script.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+
+    $("#ninja1").click(function () {
+        $("#ninjaCartoon1").fadeOut();
+    });
+
+    $("#ninja2").click(function () {
+        $("#ninjaCartoon2").fadeOut();
+    });
+
+
+    $("#ninja3").click(function () {
+        $("#ninjaCartoon3").fadeOut();
+    });
+
+
+    $("#ninja4").click(function () {
+        $("#ninjaCartoon4").fadeOut();
+    });
+
+
+    $("#ninja5").click(function () {
+        $("#ninjaCartoon5").fadeOut();
+    });
+
+
+    $("#ninja6").click(function () {
+        $("#ninjaCartoon6").fadeOut();
+    });
+
+
+    $("#ninja7").click(function () {
+        $("#ninjaCartoon7").fadeOut();
+    });
+
+
+    $("#ninja8").click(function () {
+        $("#ninjaCartoon8").fadeOut();
+    });
+
+    $('#restore').click(function(){
+        $("img").fadeIn();
+    })
+
+});
+
+
+
+
+// $("#redButton").click(function(){
+//     $("h1").addClass("redHeading");
+// });
+
+// $("#click").click(function () {
+//     alert("Neato");
+// });
+
+// $("#toggle").click(function () {
+//     $("p.toggle").toggle();
+// });
+
+// $("#slideToggle").click(function () {
+//     $("p.toggle").slideToggle();
+// });
+
+// $("#before").click(function(){
+//     $("p.toggle").before("<p>Oh, Hello!</p>");
+// });
+
+// $("#after").click(function(){
+//     $("p.toggle").after("<p>Thanks, for stopping by!</p>");
+// });
+
+// $("#append").click(function(){
+//     $("p.toggle").append("<p>Boom, appended!</p>");
+// });
+
+// $("#HTML").click(function(){
+//     $("p.toggle").html("too slow!");
+// });
+
+
+// $("#show").click(function () {
+//     $("#showP").show();
+// });
+
+
+// $("#hide").click(function () {
+//     $("#showP").hide();
+// });
+
+// $("#pictureShow").click(function() {
+//     $("#image").slideDown();
+// });
+
+// $("#pictureAway").click(function() {
+//     $("#image").slideUp();
+// });
+
+// $("#fadeIn").click(function() {
+//     $("#image").fadeIn();
+// });
+
+// $("#fadeOut").click(function() {
+//     $("#image").fadeOut();
+// });
+
+// $("#attr").click(function(){
+// $( "#image" ).attr( "title", "Photo by Kelly Clark" );
+// });
+
